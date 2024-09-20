96b8c387a1eb8732469cfbdd09e8c8f74d7cf59f
Carolina Ferreira <carolina.ferreira@poatek.com>
2024-09-20 18:41:48 -0300

    Add Mochila

--- Mochila.java
+++ Mochila.java
@@ -18,19 +18,34 @@
  *      senão
  *          maxTab[i][j] = maxTab[i-1][j];
  * retorne maxTab[N][C] // valor máximo para uma mochila de capacidade C e //que pode conter itens que vão do item 1 até o item N.
- * */
+ *
+*/
 public class Mochila {
     public static void main(String[] args) {
-        int N = 4;
-        int C = 7;
+        int[] pesos = {5, 2, 2, 1};
+        int[] valores = {2, 4, 2, 3};
+        int capacidade = 7;
 
-        int[] itens =  new int[N + 1];
-        int[][] maxTab = new int[N + 1][C + 1];
+        int maxValue = Knapsack(pesos, valores, capacidade);
+        System.out.println(maxValue);
+    }
 
-        for (int i = 1; i<=N; i++) {
-            for (int j = 1; j<=C; j++) {
-                if (itens[i].)
+    public static int Knapsack(int[] pesos, int[] valores, int capacidade) {
+        int n = pesos.length;
+        int [][] maxTab = new int[n+1][capacidade+1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= capacidade; j++) {
+                if (pesos[i - 1] > j) { //se o peso do item i-1 for maior que a capacidade, não podemos incluí-lo
+                    maxTab[i][j] = maxTab[i-1][j]; //o valor é o mesmo se não considerássemos esse item
+                } else {
+                    //podemos escolher incluir ou não o item, o que for melhor
+                    maxTab[i][j] = Math.max(maxTab[i-1][j], maxTab[i-1][j-pesos[i - 1]] + valores[i - 1]);
+                }
             }
         }
+
+        //o valor máximo estará em maxTab[n][capacidade]
+        return maxTab[n][capacidade];
     }
 }
